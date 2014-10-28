2a5c4be49a24f87e49f98322aab506a3c3250f72
lauri0 <lauri.kongas@hotmail.com>
2014-10-28 23:05:36 +0200

    Craeted visible buttons

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,5 +1,7 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include <QtGui>
+
+#include "MainWindow.h"
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
@@ -7,13 +9,47 @@ MainWindow::MainWindow(QWidget *parent) :
     setFixedSize(800, 600);
     setWindowTitle("Planning algorithms");
     initMainWindow();
+
 }
 
 MainWindow::~MainWindow()
 {
-
+    //Nothing here atm
 }
 
 void MainWindow::initMainWindow() {
+    mainLayout = new QHBoxLayout();
+    m_wdg = new QWidget();
+
+    createButtonBox();
+
+    mainLayout -> addWidget(buttonBox);
+    m_wdg -> setLayout(mainLayout);
+
+    setCentralWidget(m_wdg);
+}
+
+void MainWindow::createButtonBox() {
+    buttonBox = new QGroupBox(tr("Algorithm"));
+    QHBoxLayout *layout = new QHBoxLayout();
+
+    FCFSButton = new QPushButton(tr("FCFS"));
+    SJFButton = new QPushButton(tr("SJF"));
+    RRButton = new QPushButton(tr("RR"));
+    MLButton = new QPushButton(tr("ML"));
+
+    layout -> addWidget(FCFSButton);
+    layout -> addWidget(SJFButton);
+    layout -> addWidget(RRButton);
+    layout -> addWidget(MLButton);
+
+    buttonBox -> setLayout(layout);
+}
+
+void MainWindow::createCanvasBox() {
+    canvasBox = new QGroupBox(tr("Visual representation"));
+    QGridLayout *layout = new QGridLayout;
 
 }
+
+
--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGroupBox>
+#include <QLabel>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout><
+#include <QPushButton>
 
 namespace Ui {
 class MainWindow;
@@ -14,10 +20,28 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    void initMainWindow();
 
 private:
-    Ui::MainWindow *ui;
+
+    void initMainWindow();
+    void createButtonBox();
+    void createCanvasBox();
+    void createTextBox();
+
+    QWidget *m_wdg;
+
+    QHBoxLayout *mainLayout;
+
+    QGroupBox *buttonBox;
+    QGroupBox *canvasBox;
+    QLabel *waitingTime;
+    QPushButton *FCFSButton;
+    QPushButton *SJFButton;
+    QPushButton *RRButton;
+    QPushButton *MLButton;
+
+    QMenu *fileMenu;
+    QAction *exitAction;
 };
 
 #endif // MAINWINDOW_H
